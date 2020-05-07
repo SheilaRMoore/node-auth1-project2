f4ad393950b5a1ae50b2acc19c4eb05480b9e568
Sheila R Moore <sherexmykes@github.com>
2020-05-06 22:42:29 -0400

    mvp

---
+++ api/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const usersRouter = require('../users/user-router.js');
+const authRouter = require('../auth/auth-router.js');
+
+const server = express();
+
+const sessionConfig = {
+      name: 'ultimate warrior',
+      secret: "Be Brave and Be Strong",
+      cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+            secure: false,
+            httpOnly: true,
+      },
+      resave: false,
+      saveUninitialized: true,
+      store: new KnexSessionStore({
+            knex: require('../data/dbConfig.js'),
+            tablename: 'sessions',
+            sidfieldname: 'sid',
+            createTable: true,
+            clearInterval: 1000 * 60 * 60,
+      }),    
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+ 
+server.get('/', (req, res) => {
+      res.send(`<h2> THIS IS WORKING </h2>`)
+      
+});
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter)
+
+module.exports = server;
